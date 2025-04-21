d22c7464f60187d51cb6169930573589060eb4a9
ByDiegoBlan <diegodev750@gmail.com>
2025-04-20 18:56:38 -0600

    Se agrega el proyecto

---
+++ DevResources/src/Database/db.ts
@@ -0,0 +1,31 @@
+type Resource = {
+    id: `${string}-${string}-${string}-${string}-${string}`
+    name: string,
+    prevImage: string,
+    url: string,
+    category: string[]
+}
+
+export const resources: Resource[] = [
+    {
+        id: "1d2c9f24-5b2e-4f84-b5c7-48cb8c69985d",
+        name: "Lucide React",
+        prevImage: "/public/ResourcesIMG/LucideReact.png",
+        url: "https://lucide.dev/",
+        category: ["Icons","Frontend","React"]
+    },
+    {
+        id: "9a3f1b9d-44b5-4c8e-b7f9-56e70878c9f4",
+        name: "HeroIcons",
+        prevImage: "/public/ResourcesIMG/HeroIcons.png",
+        url: "https://heroicons.com/",
+        category: ["Icons", "Frontend"]
+    },
+    {
+        id: "9a3f1b9d-44b5-4c8e-b7f9-56e70878c9f4",
+        name: "Paletas de colores",
+        prevImage: "/public/ResourcesIMG/TiendaNube.png",
+        url: "https://paletadecolores.com.mx/",
+        category: ["Colors", "Frontend","UI"]
+    }
+];
